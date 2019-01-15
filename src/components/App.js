9ffc0056a2509cd13d6da2e05b42bc9c86ae1072
Andrew Davis <atd285@gmail.com>
2019-01-15 16:56:46 -0500

    compenent setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './Header/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header name="😺 Emoji Search 😺" />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+function Header({ name }) {
+  return (
+    <div className="emoji-header">
+      <h1 className="logo">{name}</h1>;
+    </div>
+  );
+}
+
+export default Header;
