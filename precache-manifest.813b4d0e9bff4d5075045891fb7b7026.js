051e86260c135df8baaabf9bcd527a6c21273bec
Andrew Davis <atd285@gmail.com>
2019-01-16 17:13:58 -0500

    Updates

---
+++ precache-manifest.813b4d0e9bff4d5075045891fb7b7026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3d9cf735b0ba750a28",
+    "url": "/emojis-search/static/css/main.c32d5a40.chunk.css"
+  },
+  {
+    "revision": "3a3d9cf735b0ba750a28",
+    "url": "/emojis-search/static/js/main.3a3d9cf7.chunk.js"
+  },
+  {
+    "revision": "6ebccf5798bf0d6c55e5",
+    "url": "/emojis-search/static/js/1.6ebccf57.chunk.js"
+  },
+  {
+    "revision": "b00718ae0e0eed40107c",
+    "url": "/emojis-search/static/js/runtime~main.b00718ae.js"
+  },
+  {
+    "revision": "73364f71e78d9f714563059ef5f6bcb1",
+    "url": "/emojis-search/index.html"
+  }
+];
